45f4dca4ae353c52e73d4d39ecaa9d3daab6e89c
kapskapskaps <kirilllov1402@gmail.com>
2020-12-03 17:42:21 +0300

    Format code
    Изменил отступ и добавил пробелы к "="

--- main.py
+++ main.py
@@ -14,22 +14,23 @@ age = now_date - release_date
 
 categories = collections.defaultdict(list)
 excel_wines = pn.read_excel('drinks.xlsx', usecols=['Категория',	'Название',	'Сорт',	'Цена',	'Картинка', 'Акция']).fillna(0).to_dict(orient='records')
+
 for wine in excel_wines:
 	categories[wine['Категория']].append(wine)
 
-
 env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml']))
+	loader = FileSystemLoader('.'),
+	autoescape = select_autoescape(['html', 'xml'])
+	)
 
 template = env.get_template('template.html')
 rendered_page = template.render(
-    age=age,
-    categories = categories,
+	age = age,
+	categories = categories,
 	)
 
 with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+	file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
