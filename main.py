1acb9539264bc4abfc8df9f612d9a92754e62311
kapskapskaps <kirilllov1402@gmail.com>
2020-12-10 17:03:26 +0300

    Revert "Rename variables"
    This reverts commit 8f07cf2a0833cbcc5445ba2ee4c4b65633b50ad0.

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ release_date = 1920
 age = now_date - release_date
 
 
-all_drinks = collections.defaultdict(list)
-excel_file = pn.read_excel(args.fp, usecols=['Категория',	'Название',	'Сорт',	'Цена',	'Картинка', 'Акция']).fillna(0).to_dict(orient='records')
+drinks = collections.defaultdict(list)
+get_excelfile = pn.read_excel(args.fp, usecols=['Категория',	'Название',	'Сорт',	'Цена',	'Картинка', 'Акция']).fillna(0).to_dict(orient='records')
 
-for wine in excel_file:
-	all_drinks[wine['Категория']].append(wine)
+for wine in get_excelfile:
+	drinks[wine['Категория']].append(wine)
 
 env = Environment(
 	loader = FileSystemLoader('.'),
@@ -33,7 +33,7 @@ env = Environment(
 template = env.get_template('template.html')
 rendered_page = template.render(
 	age = age,
-	drinks = all_drinks,
+	drinks = drinks,
 	)
 
 with open('index.html', 'w', encoding="utf8") as file:
